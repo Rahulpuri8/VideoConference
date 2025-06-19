558811f72ff669d8a324ac57c9bdc7e332d6aae4
RahulPuri <92353513+Rahulpuri8@users.noreply.github.com>
2025-06-19 17:32:12 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 # Audio/Video Configuration
 AUDIO_SAMPLE_RATE = 16000
 VIDEO_FPS = 25
-VAD_FRAME_MS = 30  # Must be 10, 20 or 30ms
-MIN_SPEECH_DURATION = 0.3  # seconds
+VAD_FRAME_MS = 30
+MIN_SPEECH_DURATION = 0.3
 
+# Try these codecs in order until one works
+CODEC_PRIORITY = ['mp4v', 'avc1', 'X264', 'MJPG']  # mp4v usually works everywhere
